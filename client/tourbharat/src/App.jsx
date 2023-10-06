5d47f13c6c98dd182f826d988ce1f9602825f3b7
Sagnik Saha <sagnik12347@gmail.com>
2023-10-07 03:08:59 +0530

    status section added

--- client/tourbharat/src/App.jsx
+++ client/tourbharat/src/App.jsx
@@ -1,6 +1,8 @@
 import styles from "./style"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Stats from "./components/Stats"
+//import { BrowserRouter as } from "react-router-dom"
 
 const App = () => {
   return (
@@ -14,6 +16,11 @@ const App = () => {
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero/>
+        </div>
+      </div> 
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
         </div>
       </div> 
     </div>
